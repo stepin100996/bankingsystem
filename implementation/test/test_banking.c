bbd4df0001f364fda17a4baffd95b18f83171158
Arnab Chakraborty <49164120+stepin100996@users.noreply.github.com>
2020-11-13 00:16:38 +0530

    Update test_banking.c

--- implementation/test/test_banking.c
+++ implementation/test/test_banking.c
@@ -1,1 +1,30 @@
+	#include "unity.h"
+	#include "banking.h"
 
+	void setUp()
+	{	}
+	void tearDown()
+	{	}
+	
+	void test_interest(void)
+	{
+  		TEST_ASSERT_EQUAL(160, interest(2.0,1000.0,8.0));
+  		TEST_ASSERT_EQUAL(100, interest(5.0,500.0,5.0));
+	}
+	
+	void test_fordelay(int j)
+	{
+		TEST_ASSERT_EQUAL(5,fordelay(5));
+		TEST_ASSERT_EQUAL(6,fordelay(5));
+	}
+	
+	void test_
+
+	int test_main(void)
+	{
+	  	UNITY_BEGIN();
+  	
+	  	RUN_TEST(test_interest);
+		RUN_TEST(test_fordelay);
+  		return UNITY_END();
+	}
